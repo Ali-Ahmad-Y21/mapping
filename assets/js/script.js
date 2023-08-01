bf98249feb1da7e90e6400905f63aa62d5721987
ali ahmad <aliahmad1221e@gmail.com>
2023-08-01 20:56:11 +0300

    e

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
-let username = ["admin01","admin02","admin03",]
+// console.log(username)
+// username.push("admin04")
+// console.log(username)
+// username.push("admin05")
+// console.log(username)
+// username.push("admin06")
+// console.log(username)
+// username.pop()
+// console.log(username)
+// username.unshift("admin00") 
+// console.log(username)
+// username.shift("admin00")
+// console.log(username)
+const addBtnEL = document.getElementById("addBtn")
+const userListEl = document.getElementById("userList")
+const addToListEl = document.getElementById("addToList")
+let username = []
 
-
-console.log(username)
-
-username.push("admin04")
-console.log(username)
-username.push("admin05")
-console.log(username)
-username.push("admin06")
-console.log(username)
-username.pop()
-console.log(username)
-username.unshift("admin00")
-console.log(username)
-username.shift("admin00")
-console.log(username)
-
+addBtnEL.addEventListener("click", function(){
+    username = []
+    let newInput = addToListEl.value 
+    username.push(newInput)
+    for (let i in username){
+        userListEl.innerHTML +=
+        `<li style="margin-left:20px">${username[i]}</li>`
+    }
+    addToListEl.value = ''
+})
